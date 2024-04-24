c2c75875a5471580645e826dd757a7bf808e4728
Zkrallah <muhammad.heshamyt@gmail.com>
2024-04-24 14:56:27 +0200

    disable eslint

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,10 +2,7 @@
 export default function getFullResponseFromAPI(success) {
 	const promise =  new Promise((resolve, reject) => {
 		if (success) {
-			resolve({
-				status: 200,
-				body:`success`
-			});
+			resolve({ status: 200, body:'success' });
 		} else {
 			reject(new Error('The fake API is not working currently'));
 		}
