5d0a3ae0185aee417b155e541f5b69c505e029c9
Zkrallah <muhammad.heshamyt@gmail.com>
2024-05-04 04:24:49 +0300

    Get List

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+    return [
+        {
+            id: 1,
+            firstName: 'Guillaume',
+            location: 'San Francisco'
+        },
+        {
+            id: 1,
+            firstName: 'Guillaume',
+            location: 'San Francisco'
+        },
+        {
+            id: 1,
+            firstName: 'Guillaume',
+            location: 'San Francisco'
+        }
+    ];
+}
